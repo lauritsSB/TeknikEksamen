0bd00e4ac0de95f5fbc963949547701ee2e522c5
lauritsSB <59605059+lauritsSB@users.noreply.github.com>
2020-04-02 13:32:16 +0200

    dialogue done

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
 
     private Queue<string> sentences;
 
@@ -17,6 +20,29 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+//        DisplayNextSentence();
+    }
+    public void DisplayNextSentence ()
+    {
+        if(sentences.Count==0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    void EndDialogue()
+    {
+        Debug.Log("End og conversation.");
     }
 }
--- Assets/DialogueTrigger.cs
+++ Assets/DialogueTrigger.cs
@@ -7,18 +7,34 @@ public class DialogueTrigger : MonoBehaviour
 
     public Dialogue dialogue;
     bool dialogTriggered;
+    DialogueManager dm;
+
+    private void Start()
+    {
+        dm = FindObjectOfType<DialogueManager>();
+    }
     public void TriggerDialogue ()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        dm.StartDialogue(dialogue);
     }
+
+    public GameObject dialogueWindow;
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" && dialogTriggered == false && Input.GetKey(KeyCode.E))
+        if (other.tag == "Player" && dialogTriggered == false && Input.GetKeyDown(KeyCode.E))
         {
+            dialogueWindow.SetActive(true);
             dialogTriggered = true;
             TriggerDialogue();
         }
+        if(other.tag=="Player" && dialogTriggered==true && Input.GetKeyDown(KeyCode.E))
+        {
+            dm.DisplayNextSentence();
+        }
     }
-
+    private void OnTriggerExit(Collider other)
+    {
+        dialogueWindow.SetActive(false);
+    }
 }
